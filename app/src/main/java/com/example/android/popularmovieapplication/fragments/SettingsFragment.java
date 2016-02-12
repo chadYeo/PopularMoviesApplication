c79a6b7a9b9298a3d3254ca3b567e95d6a9e0142
Chad Yeo <chad.yeo.cy@gmail.com>
2016-02-12 15:00:24 -0500

    Added all fragments, layouts, menu, and xml

---
+++ app/src/main/java/com/example/android/popularmovieapplication/fragments/SettingsFragment.java
@@ -0,0 +1,32 @@
+package com.example.android.popularmovieapplication.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.popularmovieapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends Fragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
